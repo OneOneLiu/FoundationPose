cafaca6286dd839aa95c0bb925ac9a2a4b4e82b5
1080 Desktop <daohui.liu@mail.utoronto.ca>
2024-05-23 20:15:22 -0400

    Fix cannot find display

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,20 @@
 docker rm -f foundationpose
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+xhost +
+docker run -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="XAUTHORITY=$XAUTH" \
+    --env NVIDIA_DISABLE_REQUIRE=1 \
+    --gpus all \
+    --network=host \
+    --name foundationpose  \
+    --cap-add=SYS_PTRACE \
+    --security-opt seccomp=unconfined \
+    -v $DIR:$DIR \
+    -v /home:/home \
+    -v /mnt:/mnt \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /tmp:/tmp  \
+    --ipc=host \
+    -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
